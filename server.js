80ae80bd5b8f80e88143752e28a87a34caec9785
Prashanta Shrestha <prashantashrestha@yahoo.com>
2018-04-08 10:25:36 -0500

    Reduce home page image size, remove halo around image blur

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HotelListComponent
   ],
   imports: [
     BrowserModule
